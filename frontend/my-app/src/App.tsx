11c2376dffba7e60eac8dd23a7345743fcfd60ca
Alf.Klahaug <alfbjarne.klehaug@gmail.com>
2025-07-15 15:09:40 +0200

    add button to test communication between backend

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [serverResponse, setRes] = useState('');
+
+  const getResponse = async () => {
+    const reponse = await axios.get('/').then(res => {
+      setRes(res.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getResponse}>Test server</button>
+      <p>{serverResponse}</p>
     </div>
   );
 }
