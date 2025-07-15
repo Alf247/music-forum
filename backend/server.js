9770d6e92afa11e583757adb60c40b5441ade155
Alf.Klahaug <alfbjarne.klehaug@gmail.com>
2025-07-15 15:26:05 +0200

    added /auth and /callback

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,54 @@ app.get('/', (req, res) => {
     } */
 })
 
+app.get('/auth', (req, res) => {
+    const scopes = [
+        'user-read-private'
+    ]
+
+    res.send(spotifyApi.createAuthorizeURL(scopes, "", true))
+})
+
+app.get('/callback', (req, res) => {
+    console.log(req.query)
+    const error = req.query.error;
+    const code = req.query.code;
+    const state = req.query.state;
+
+    if (!code) {
+        console.error('No authorization code received.');
+        return res.status(400).send('Authorization code is missing.');
+    }    
+
+    if (error) {
+        console.error('Error when getting callback: ', error);
+        res.send(`Error when getting callback: ${error}`)       // Preventes end-point from constantly waiting
+        return;
+    }
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        const accessToken = data.body['access_token'];
+        const refreshToken = data.body['refresh_token'];
+        const expiresIn = data.body['expires_in'];
+
+        spotifyApi.setAccessToken(accessToken);
+        spotifyApi.setRefreshToken(refreshToken);
+
+        console.log(accessToken + "\n\n" + refreshToken);
+        //res.redirect(`../dashboard/?access_token=${accessToken}&refresh_token=${refreshToken}`);
+
+        setInterval(spotifyApi.refreshAccessToken().then(data => {
+            console.log('The access token has been refreshed!');
+            spotifyApi.setAccessToken(data.body['access_token']);
+        }).catch(error => {
+            console.log('Could not refresh access token: ', error);
+        }), expiresIn / 2*1000);
+    }).catch(error => {
+        console.error('\nError authorizationCodeGrant: ', error);
+        res.send('Error getting token.')
+    })
+});
+
 app.listen(port, () => {
     console.log("Server started!")
 })
