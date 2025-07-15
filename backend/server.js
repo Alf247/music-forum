e85ab972aa237579cbb63189919224ea9bad4ade
Alf.Klahaug <alfbjarne.klehaug@gmail.com>
2025-07-15 14:21:04 +0200

    trying to run server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const https = require('https')
 const router = app.router
 const SpotifyWebApi = require('spotify-web-api-node')
 
-/* const spotifyApi = new SpotifyWebApi({
-    clientId:''
-}); */
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: 'https://music-forum.onrender.com/callback'
+});
 
 const MY_ACCESS_TOKEN = 'MY_ACCESS_TOKEN';
 
@@ -16,4 +19,6 @@ app.get('/', (req, res) => {
     }
 })
 
-//app.listen
+const server = https.createServer(app)
+
+server.listen()
