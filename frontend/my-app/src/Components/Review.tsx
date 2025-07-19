08a7d215736baae79de3a32b7a3c9c5ae41bef79
Alf.Klahaug <alfbjarne.klehaug@gmail.com>
2025-07-19 19:40:44 +0200

    added review component

--- frontend/my-app/src/Components/Review.tsx
+++ frontend/my-app/src/Components/Review.tsx
@@ -1,23 +1,47 @@
+import axios from "axios"
 import { useEffect, useState } from "react"
 
-function Review(reviewId: string) {
+interface ReviewI {
+    user_id: string,
+    album_id: string,
+    rating: number,
+    reviewText: string,
+    created_at: string
+}
 
-    const [user, setUser] = useState()
-    const [review, setReview] = useState()
+function Review(review: ReviewI) {
+
+    const {user_id, album_id, rating, reviewText, created_at} = review
+
+    const [user, setUser] = useState<any>(null)
+    const [album, setAlbum] = useState<any>(null)
 
     useEffect(() => {
-        // Get me
-        // Get review
-    }, [])
+        // Get user
+        axios.get('/user', { params: { user_id: user_id } }).then(res => {
+            setUser(res.data)
+            console.log('Got user information.')
+        }).catch(err => {
+            console.error('Error getting /user: ', err)
+        })
+
+        // Get album
+        axios.get('/album', { params: { album_id: album_id } }).then(res => {
+            setAlbum(res.data)
+            console.log('Got album: ', album)
+        }).catch(err => {
+            console.log('Error getting album: ', err)
+        })
+    }, [album, album_id, review.user_id, user_id])
 
     return (
         <div className="review">
-            <img src="ALBUM COVER" alt="" />
+            <img src="ALBUM COVER" alt="Cover of album" />
             <div className="review-body">
-                <h1 className="album-title">PLACEHOLDER</h1>
-                <time dateTime="">PLACEHOLDER</time>
-                <p className="review-rating">PLACEHOLDER</p>
-                <p className="review-text">PLACEHOLDER</p>
+                <h1 className="album-title">{album}</h1>
+                <p className="date">{created_at}</p>
+                <p className="review-rating">{rating}</p>
+                <p className="review-text">{reviewText}</p>
                 <button>Edit</button>
 
             </div>
--- frontend/my-app/src/Home.tsx
+++ frontend/my-app/src/Home.tsx
@@ -3,15 +3,17 @@ import { useEffect, useState } from "react";
 import Me from "./Me";
 import Auth from "./components/Auth";
 import Header from "./components/Header";
+import Review from "./components/Review";
 
 
 function Home() {
 
-    const [serverResponse, setRes] = useState('')
+    const [reviews, setReviews] = useState([])
     const [authorized, setAuthorized] = useState(false)
 
     useEffect(() => {
         checkAuthorization()
+        getReviews()
     }, [])
 
     const checkAuthorization = () => {
@@ -22,19 +24,28 @@ function Home() {
         })
     }
 
-    const getResponse = () => {
-        axios.get('/').then(res => {
-            setRes(res.data)
+    const getReviews = () => {
+        axios.get('/reviews').then(res => {
+            setReviews(res.data)
         }).catch(err => {
-            console.log(err)
+            console.error('Error getting all reviews: ', err)
         })
     }
 
-    
     return (
         authorized ?
             <>
                 <Header/>
+                {reviews.map((item, index) => (
+                    <Review 
+                        key={index}
+                        user_id={item['user_id']}
+                        album_id={item['album']}
+                        rating={item['rating']}
+                        reviewText={item['review']}
+                        created_at={item['created_at']}
+                    />
+                ))}
                 <Me></Me>
                 <br />
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum unde dolores odit vitae alias vel laudantium similique in aspernatur magni aliquam distinctio id quod maxime neque aliquid, repellat nemo sint? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum saepe hic fugit vero, non unde voluptatum placeat earum deserunt eius inventore quia dignissimos sed ratione at corrupti alias repellat quaerat.
