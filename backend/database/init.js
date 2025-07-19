1ef8531c28d21eebce6fb31e03dae8aabf5bfaac
Alf.Klahaug <alfbjarne.klehaug@gmail.com>
2025-07-19 15:48:45 +0200

    updated init to use promises

--- backend/database/init.js
+++ backend/database/init.js
@@ -1,20 +1,30 @@
-const fs = require('fs')
+const fs = require('fs').promises
 const path = require('path')
 const pool = require('./database')
 
 const initializeFromSQL = async () => {
-    try {
-        const schema = await fs.readFile(
-            path.join(__dirname, './schema.sql'),
-            'utf8'
-        )
-
-        await pool.query(schema)
+    fs.readFile(path.join(__dirname, './schema.sql'), 'utf8')
+        .then(schema => {
+            pool.query(schema)
+            console.log('Database schema initialized.')
+        }).catch(error => {
+            console.error('Failed to initialize SQL schema: ', error)
+            throw error
+        }
+    )
+        
+        /* try {
+            const schema = fs.readFile(
+                path.join(__dirname, './schema.sql'),
+                'utf8'
+            )
+            
+        pool.query(schema)
         console.log('Database schema initialized.')
     } catch (error) {
         console.error('Failed to initialize SQL schema: ', error)
         throw error
-    }
+    } */
 }
 
 module.exports = initializeFromSQL
