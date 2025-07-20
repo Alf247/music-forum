1a81abc7f3d6cbf8cc3fe7790b7edcbb2c8af9ea
Alf.Klahaug <alfbjarne.klehaug@gmail.com>
2025-07-20 18:52:13 +0200

    Updated reviews to show the latest first, removed border around album cover

--- backend/database/query.js
+++ backend/database/query.js
@@ -1,3 +1,3 @@
-export const everything = 'SELECT * FROM reviews;'
+export const everything = 'SELECT * FROM reviews ORDER BY created_at DESC;'
 
 export const submitReview = 'INSERT INTO reviews (user_id, album, review, rating) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING;'
